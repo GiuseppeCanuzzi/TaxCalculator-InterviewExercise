14c8c814e240447d21010556defd8c1d5086c88a
giuseppecanuzzi <giuseppecanuzzi@192.168.1.107>
2017-09-09 01:17:22 +0200

    Another try to start travis

--- TaxCalculator-InterviewExercise/src/test/java/LibraryTest.java
+++ TaxCalculator-InterviewExercise/src/test/java/LibraryTest.java
@@ -8,6 +8,6 @@ public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-        assertTrue(true);
+        assertFalse(false);
     }
 }
