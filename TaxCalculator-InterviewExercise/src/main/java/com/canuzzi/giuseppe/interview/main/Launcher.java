14d1f7c18c5d7f2318d6b36304cfb1831973b248
giuseppecanuzzi <giuseppecanuzzi@192.168.1.107>
2017-09-09 14:31:24 +0200

    Added Comments

--- TaxCalculator-InterviewExercise/src/main/java/com/canuzzi/giuseppe/interview/main/Launcher.java
+++ TaxCalculator-InterviewExercise/src/main/java/com/canuzzi/giuseppe/interview/main/Launcher.java
@@ -4,6 +4,13 @@ import com.canuzzi.giuseppe.interview.dal.FakeDataSourceFactory;
 import com.canuzzi.giuseppe.interview.dal.IDataSource;
 import com.canuzzi.giuseppe.interview.dal.InputType;
 
+/**
+ * This is the main class where i setup everything i need to run the
+ * tax calculator controller
+ * 
+ * @author giuseppecanuzzi
+ *
+ */
 public class Launcher {
 
 	public static void main(String[] args) {
@@ -12,11 +19,12 @@ public class Launcher {
 		//TODO Retrieve goods from cart or other data sources (db)
 		IDataSource dataSource = FakeDataSourceFactory.getCart(InputType.FIRST);
 		
+		
 		//TODO Create a controller for tax calculation, the controller could have a reference to an outputter object (i.e a view, printer)
 		
+		//Single point of injection for this tax calculator controller
+		//TODO Adding Guice to manage dependency injection could centralize config even more for future project growth
 		
-		
-		//TODO Print the final receipt
 	}
 
 }
